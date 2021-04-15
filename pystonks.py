04df7c37de357bf9c07a5dc91ddc7b0cb4c55ef0
Yoong Jia Hui <yoongjh95@outlook.sg>
2021-04-15 21:57:16 +0800

    Endless loop test, prints out open and close values

--- pystonks.py
+++ pystonks.py
@@ -4,6 +4,7 @@ import datetime
 import math
 from stockalarm import StockAlarm 
 import time
+import numpy as np
 
 
 now = datetime.datetime.now().date()
@@ -15,13 +16,22 @@ data = yf.download(tickers = stonkList, period='1d', interval = "1m",start = fri
 print(data)
 
 test_list = "PLTR GME CRSR AMD INTC NVDA"
+split_test_list = test_list.split()
 while True:
     refresh_Data = yf.download(tickers = test_list, period='1d', interval = "1m")
-    print(refresh_Data.loc[refresh_Data.index[-1], 'Close'])
+    print(str(refresh_Data.iloc[-1].name))      # Print timestamp
+    for stonk in split_test_list:
+        # Check if Open value of stonk is real number
+        if not np.isnan(refresh_Data.iloc[-1]["Open"][stonk]):
+            print("Stonk: {} -> Open  : {}".format(str(stonk), str(refresh_Data.iloc[-1]["Open"][stonk])))
+        # Check if Close value of stonk is real number
+        if not np.isnan(refresh_Data.iloc[-1]["Close"][stonk]):
+            print("Stonk: {} -> Close : {}".format(str(stonk), str(refresh_Data.iloc[-1]["Close"][stonk])))
     time.sleep(10)
     # To-do: Break down dataframe information into lists per stock
     # To-do: Save a local csv file periodically with timestamps (one file per stock?)
     # To-do: Filter out NaNs from latest downloads and only print out real numbers
+    # # Sub-task: Find most recent real number if latest is nan
 #-------------- Example --------------#
 
 # NXE = yf.Ticker("NXE")
